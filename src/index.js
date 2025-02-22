2ebf17b5e911eea6bef176d83b4565411e2c5cef
project-demo <kumarikomal62057@gmail.com>
2025-02-23 01:49:30 +0530

    New project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+// import { ReactDOM } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="Komal Kumari.jpg" alt="Komal Kumari" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Komal Kumari</h1>
+      <p>
+        My name is Komal Kumari student of btech final year . I like to play
+        games and do coding in my free time
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill Skill="React" color="blue" />
+      <Skill Skill="HTML+CSS" color="orangered" />
+      <Skill Skill="JS" color="yellow" />
+      <Skill Skill="C++" color="green" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="Skill" style={{ backgroundColor: props.color }}>
+      <span>{props.Skill}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
